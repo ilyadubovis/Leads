37bcfd596e9d889cea262ab7dcb1447711848d1a
Ilya Dubovis <idubovis@hps.md>
2025-01-29 21:15:30 -0600

    cleanup

--- leads.client/src/app/app.component.ts
+++ leads.client/src/app/app.component.ts
@@ -1,28 +1,11 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { environment } from '../environments/environment';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    
-  }
-
-  
+export class AppComponent {
 
   title = 'leads.client';
 }
